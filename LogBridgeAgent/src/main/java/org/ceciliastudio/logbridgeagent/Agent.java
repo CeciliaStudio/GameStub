6a7cc6e0ad01b6c790721cba09a416795bb043af
AnemoFlower <yizhimcqiu@icloud.com>
2026-03-10 18:45:34 +0800

    feat: Send stdout/stderr to the Unix Domain Socket specified in agentArgs

--- LogBridgeAgent/src/main/java/org/ceciliastudio/logbridgeagent/Agent.java
+++ LogBridgeAgent/src/main/java/org/ceciliastudio/logbridgeagent/Agent.java
@@ -1,9 +1,38 @@
 package org.ceciliastudio.logbridgeagent;
 
+import java.io.IOException;
+import java.io.PrintStream;
 import java.lang.instrument.Instrumentation;
+import java.net.StandardProtocolFamily;
+import java.net.UnixDomainSocketAddress;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Agent {
-    public static void premain(String args, Instrumentation inst) {
-        System.out.println("[GameStub LogBridge] Premain loaded. args: " + args);
+
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println("[GameStub LogBridge] Premain loaded. agentArgs: " + agentArgs);
+        Path socketPath = Path.of(agentArgs);
+        if (!Files.exists(socketPath)) {
+            System.err.println("[GameStub LogBridge] the socket file does not exists: " + socketPath);
+            System.exit(40);
+        }
+
+        try {
+            SocketChannel socket = createSocket(socketPath);
+            System.setOut(new PrintStream(new BridgedOutputStream(socket, System.out, false)));
+            System.setErr(new PrintStream(new BridgedOutputStream(socket, System.err, true)));
+        } catch (IOException e) {
+            System.err.println("[GameStub LogBridge] Failed to create socket: " + e.getMessage());
+        }
+    }
+
+    private static SocketChannel createSocket(Path path) throws IOException {
+        UnixDomainSocketAddress address = UnixDomainSocketAddress.of(path);
+        SocketChannel socketChannel = SocketChannel.open(StandardProtocolFamily.UNIX);
+        socketChannel.configureBlocking(false);
+        socketChannel.connect(address);
+        return socketChannel;
     }
 }
---
+++ LogBridgeAgent/src/main/java/org/ceciliastudio/logbridgeagent/BridgedOutputStream.java
@@ -0,0 +1,39 @@
+package org.ceciliastudio.logbridgeagent;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class BridgedOutputStream extends OutputStream {
+    private final SocketChannel channel;
+    private final OutputStream standardOutputStream;
+    private final boolean isError;
+    private final ByteBuffer buffer;
+
+    public BridgedOutputStream(SocketChannel channel, OutputStream standardOutputStream, boolean isError) {
+        this.channel = channel;
+        this.standardOutputStream = standardOutputStream;
+        this.isError = isError;
+        this.buffer = ByteBuffer.allocate(1024);
+        this.buffer.put((byte) (isError ? 0x01 : 0x00));
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        this.standardOutputStream.write(b);
+        this.buffer.put((byte) b);
+        if (b == '\n') {
+            this.buffer.flip();
+            this.channel.write(this.buffer);
+            this.buffer.clear();
+            this.buffer.put((byte) (isError ? 0x01 : 0x00));
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.standardOutputStream.close();
+        this.channel.close();
+    }
+}
